b1fc471ebfab3e5f975bcb9126c2352a46abcf68
wendecoder <wenonezra258@gmail.com>
2022-06-02 09:30:40 -0700

    Importing modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 if __name__ == '__main__':
-    import add_0
+    from add_0 import add
 
     a = 1
     b = 2
 
-    print("{:d} + {:d} = {:d}".format(1, 2, add_0.add(1, 2)))
+    print("{:d} + {:d} = {:d}".format(1, 2, add(1, 2)))
