e3f99f9fbe510f4df52c15bca584636aacb1b3c6
wendecoder <wenonezra258@gmail.com>
2022-06-02 12:31:16 -0700

    importing modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,14 +2,14 @@
 def infinite_add(nums):
     inputs = len(nums) - 1
     if inputs == 0:
-        print(0)
+        print("{:d}".format(inputs))
     else:
         j = 1
         sum = 0
         while j <= inputs:
             sum += int(nums[j])
             j += 1
-        print(sum)
+        print("{:d}".format(sum))
 if __name__ == "__main__":
     import sys
     infinite_add(sys.argv)
