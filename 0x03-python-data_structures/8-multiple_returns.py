aa3fff7cfd8bf4d501694eab764147a405a75a72
wendecoder <wenonezra258@gmail.com>
2022-06-05 16:53:00 -0700

    hbue

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        sentence[0] = None
-        tuple_a = len(sentence), sentence[0]
+        tuple_a = len(sentence), None
         return tuple_a
     else:
         tuple_a = len(sentence), sentence[0]
