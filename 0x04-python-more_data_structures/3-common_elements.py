e4ed5b68cadf325d4f2de47c2a93aafa8211799b
wendecoder <wenonezra258@gmail.com>
2022-06-08 15:10:48 -0700

    nfsj

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    common_elem = ""
-    for q, v in zip(set_1, set_2):
-        if q == v:
-            common_elem = q
-    return common_elem
+    return(set_1 & set_2) 
