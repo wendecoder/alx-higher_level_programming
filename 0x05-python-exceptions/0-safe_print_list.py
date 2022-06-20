957127b1c17f31d3dda6a9f6163c7352d82b174b
wendecoder <wenonezra258@gmail.com>
2022-06-20 08:21:06 -0700

    Exception handling

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,9 +6,9 @@ def safe_print_list(my_list=[], x=0):
             if index < x:
                 print(my_list[index], end='')
                 index += 1
+            else:
+                print()
+                return index
         except IndexError:
-            print()
-            return index
-        else:
             print()
             return index
