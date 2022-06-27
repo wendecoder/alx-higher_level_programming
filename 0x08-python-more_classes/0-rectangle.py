4e2d4c22511703938009fc56a1b075149cacb6ed
wendecoder <wenonezra258@gmail.com>
2022-06-27 08:37:27 -0700

    more classes

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+It is a Rectangle class.
+"""
+
+
+class Rectangle:
+    """
+    Creating an empty Rectangle Object.
+    """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+more classe and objects
