fa2d8adfe0cee82f718e88277c83357eb432b146
wendecoder <wenonezra258@gmail.com>
2022-09-06 16:01:29 +0300

    fixed square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 const myArg = parseInt(process.argv[2]);
+let str = '';
 if (myArg === undefined || isNaN(myArg)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < myArg; i++) {
-    let str = '';
+    str = '';
     for (let j = 0; j < myArg; j++) str += 'X';
     console.log(str);
   }
