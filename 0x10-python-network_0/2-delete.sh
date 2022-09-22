797eb43b672967a259475b4eea949d3b8ddf6f0a
wendecoder <wenonezra258@gmail.com>
2022-09-23 02:37:48 +0300

    done 5th

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # curl command that sends request with delete method
-curl -sfL "$1" -X DELETE
+curl -s "$1" -X DELETE
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays all HTTP methods the server accepts
-curl -sI "$1" | grep Allow | awk '{print2}'
+curl -sI "$1" | grep Allow | cut -d ' ' -f2-
