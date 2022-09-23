514f49a782d8969f3eadcb31bdffd47ec0aa0428
wendecoder <wenonezra258@gmail.com>
2022-09-23 03:24:22 +0300

    done 4th

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends GET request with header variable 'X-School-User-Id' and it value 98
-curl -sfL -H "X-School-User-Id: 98" "$1" -X GET
+curl -sL -H "X-School-User-Id: 98" "$1" -X GET
