80a1607ff3ed39c0513723994ac919376c89f3e2
wendecoder <wenonezra258@gmail.com>
2022-09-23 13:43:31 +0300

    hbtn_status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+fetches https://alx-intranet.hbtn.io/status
+using urllib package and also using with statement
+and give output in the following form
+Body response:$
+    - type: <class 'bytes'>$
+    - content: b'OK'$
+    - utf8 content: OK$
+"""
+import urllib.request
+
+if __name__ == '__main__':
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as Response:
+        response = Response.read()
+        print("Body response:$")
+        print("\t- type: {}$".format(type(response)))
+        print("\t- content: {}$".format(response))
+        print("\t- utf8 content: {}$".format(response.decode('UTF-8')))
