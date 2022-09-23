7f9c0b30e93cbb1893ece86ecaa41ba5a7afda69
wendecoder <wenonezra258@gmail.com>
2022-09-24 02:08:00 +0300

    done

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,13 +5,14 @@ the body of the response (decoded in utf-8) while
 managing the error exception using urllib.error.HTTPError
 """
 
-import urllib
+import urllib.request
 import sys
 
 if __name__ == '__main__':
     try:
         url = sys.argv[1]
-        with urllib.request.urlopen(url) as Response:
+        request = urllib.request.Request(url)
+        with urllib.request.urlopen(request) as Response:
             print(Response.read().decode('UTF-8'))
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
