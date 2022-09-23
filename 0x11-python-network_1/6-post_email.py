f6794752141f6d4d2cc7cb5e36ff27a003d7444f
wendecoder <wenonezra258@gmail.com>
2022-09-24 02:57:15 +0300

    fixed

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -11,6 +11,6 @@ import sys
 if __name__ == '__main__':
     url = sys.argv[1]
     data = {'email': sys.argv[2]}
-    
+
     response = requests.post(url, data)
     print(response.text)
--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,9 +9,8 @@ import sys
 
 if __name__ == '__main__':
     url = sys.argv[1]
-    try:
-        response = requests.get(url)
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
         print(response.text)
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as e:
-        print("Error code: {}".format(e.code)
