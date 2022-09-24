af4640e013416e8647f0e796d6a2aac7db2bf1b0
wendecoder <wenonezra258@gmail.com>
2022-09-24 05:59:10 +0300

    done

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Takes repository name and owner name and then prints
+latest 10 commits
+"""
+
+import sys
+import requests
+
+if __name__ == '__main__':
+    reposit = sys.argv[1]
+    owner = sys.argv[2]
+
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, reposit)
+    request = requests.get(url)
+    res = res.json()
+    try:
+        for i in range(10):
+            print('{}: {}'.format(
+                res[i].get('sha'),
+                res[i].get('commit').get('author').get('name')))
+    except IndexError:
+        pass
